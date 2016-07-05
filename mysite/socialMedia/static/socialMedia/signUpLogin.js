8ef40fdf64847334be397623954bdf7fd5d1b30c
donowhy <donowhy11@gmail.com>
2016-07-05 14:14:57 -0700

    zxc

--- mysite/socialMedia/static/socialMedia/signUpLogin.js
+++ mysite/socialMedia/static/socialMedia/signUpLogin.js
@@ -13,6 +13,7 @@ $("#cancelSignUp").click(function(){
 });
 
 $("#submitSignUp").click(function() {
+    $("#errorCode").empty();
     //gather info
     var firstName = $("#signUpFirstName").val() || null;
     var lastName = $("#signUpLastName").val() || null;
@@ -23,19 +24,19 @@ $("#submitSignUp").click(function() {
     //validate info
     console.log(firstName);
     if(firstName == null){
-        $("#errorCode").append("Please fill in your first name!");
+        $("#errorCode").append("<p>Please fill in your first name!</p>");
     }
     if(lastName == null){
-        $("#errorCode").append("Please fill in your last name!");
+        $("#errorCode").append("<p>Please fill in your last name!</p>");
     }
     if(email == null){
-        $("#errorCode").append("Please fill in your email!");
+        $("#errorCode").append("<p>Please fill in your email!</p>");
     }
     if(passCon == null){
-        $("#errorCode").append("Please fill in your password!");
+        $("#errorCode").append("<p>Please fill in your password!</p>");
     }
     if(passCon != pass){
-        $("#errorCode").append("Error: Passwords do not match");
+        $("#errorCode").append("<p>Error: Passwords do not match</p>");
     }
     var userData = [firstName, lastName, email, passCon];
     //ajax request to server
