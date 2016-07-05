4c3b6c0b8d1a4f34281e98bb01010e5b27017f33
donowhy <donowhy11@gmail.com>
2016-07-05 13:56:57 -0700

    zxc

--- mysite/socialMedia/static/socialMedia/signUpLogin.js
+++ mysite/socialMedia/static/socialMedia/signUpLogin.js
@@ -1,9 +1,13 @@
 $("#signUpButton").click(function(){
     $("#main1").hide();
+    $("#join1").hide();
+    $("#join2").show();
     $("#main2").show();
 });
 
 $("#cancelSignUp").click(function(){
     $("#main2").hide();
+    $("#join2").hide();
+    $("#join1").show();
     $("#main1").show();
 });
